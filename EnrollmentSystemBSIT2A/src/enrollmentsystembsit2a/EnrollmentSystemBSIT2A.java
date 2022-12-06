5fe3470a7d454bfdac51e5de56dd5e411ddce4e9
Justin Froa Pilongo <justinfroapilongo@gmail.com>
2022-12-06 17:29:18 +0800

    Update EnrollmentSystemBSIT2A.java

--- EnrollmentSystemBSIT2A/src/enrollmentsystembsit2a/EnrollmentSystemBSIT2A.java
+++ EnrollmentSystemBSIT2A/src/enrollmentsystembsit2a/EnrollmentSystemBSIT2A.java
@@ -1,44 +1,47 @@
 package enrollmentsystembsit2a;
+import java.awt.Color;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class EnrollmentSystemBSIT2A {
 
     public static void main(String[] args) {
         
-        //Frames
-        JFrame container = new JFrame();
+        //Images
+        ImageIcon logo = new ImageIcon("D:\\Programming\\git\\enrollment-system\\EnrollmentSystemBSIT2A\\src\\assets\\circle_crop_background.png");
+        Icon logoApp = new ImageIcon("src/assets/enrollment_system_transparent_logo_circle.png");
+        
+        //Borders
+        Border border = BorderFactory.createLineBorder(new Color(0x413861), 10);
+        
+        //Frames (Layout of the Application)
+        JFrame layout = new JFrame("St. Anne's Catholic School Enrollment System");
+        layout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        layout.setSize(500, 500);
+        
+        //Logo & Title
+        layout.setIconImage(logo.getImage());
         
         //Labels
         JLabel label = new JLabel();
-        
-        //Images
-        ImageIcon logo = new ImageIcon("D:\\Programming\\git\\enrollment-system\\EnrollmentSystemBSIT2A\\src\\assets\\circle_crop_background.png");
-        ImageIcon logoApp = new ImageIcon("D:\\Programming\\git\\enrollment-system\\EnrollmentSystemBSIT2A\\src\\assets\\enrollment_system_transparent_logo_circle.png");
-        
-        //Icon Logo of the Application
-        container.setIconImage(logo.getImage());
-        
-        //Title of the Application
-        container.setTitle("St. Anne's Catholic School Enrollment System");
-        
-        //Function of the Application
-        container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        container.setSize(500, 500);
-        container.setVisible(true);
-        
-        //Interface of the Applicaiton
-        label.setIcon(logoApp);
-        label.setText("St. Anne's Catholic School Enrollment System");
-        
-        //Changes Text Position
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
         label.setVerticalTextPosition(JLabel.BOTTOM);
         label.setHorizontalTextPosition(JLabel.CENTER);
         
-        //Changes Alignments of Objects
-        label.setVerticalAlignment(JLabel.CENTER);
-        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setIcon(logoApp);
+        label.setText("St. Anne's Catholic School Enrollment System");
         
-        container.add(label);
+        //Panels (Container for the GUI)
+        JPanel startPanel = new JPanel();
+        startPanel.add(label);
+        startPanel.setBorder(border);
+        startPanel.setBackground(new Color(200, 200, 200));
+        
+        //Displaying GUI to Application
+        layout.setVisible(true);
+        layout.add(startPanel);
+        
         
     }
     
